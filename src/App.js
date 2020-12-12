0f744a802336b41cb85a77987ac22dbc0c6c3e14
Samanja Cartagena <70960407+SamanjaCartagena@users.noreply.github.com>
2020-12-11 23:03:02 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import logo from './logo.svg';
 import './App.css';
+import  './Header';
 
 function App() {
   return (
     <div className="App">
      <h1>Samanja</h1>
      <p>weuhkfgkjsdhgfjsdagfjgdsfjhgsjdhfgjshdgfjhdsgfjhsdgfjhg</p>
-     <Header/>
+     
     </div>
   );
 }
