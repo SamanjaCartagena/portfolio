3e0560d22df9f4f3da4757ce81b96a901492e9d1
Samanja Cartagena <70960407+SamanjaCartagena@users.noreply.github.com>
2021-01-28 20:20:50 -0500

    fresh start

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ import  './Header';
 function App() {
   return (
     <div className="App">
-     <h1>Samanja</h1>
-     <p>weuhkfgkjsdhgfjsdagfjgdsfjhgsjdhfgjshdgfjhdsgfjhsdgfjhg</p>
-     <p>Port changed to 80</p>
-     <h1>Netlify</h1>
-     <h1>Hi</h1>
      
     </div>
   );
