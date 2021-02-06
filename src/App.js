7e727d46540f413bc169ba8ab3c1a092e026372b
Samanja Cartagena <70960407+SamanjaCartagena@users.noreply.github.com>
2021-02-06 11:44:03 -0500

    remove Borat

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import logo from './logo.svg';
 import './App.css';
-import  './Header';
 import React,{Image} from 'react'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi my name is Borat, my friend Samanja, shes very nice. Hi Five!!<br/></h1>
-    <img src="Sacha-Baron-Cohen-as-Borat.jpg"/>
+      <div className="samanjaName">
+      <hr/>
+      </div>
 
     </div>
   );
