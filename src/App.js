6654383e6aaf040e59df7c4f468f04a529fbf6b9
SamanjaCartagena <cart.samanja09@gmail.com>
2022-10-12 12:44:42 -0400

    hero image added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import NavBar from "./components/NavBar";
+import Home from './components/Home';
 
 function App() {
   return (
     <h1 className="">
       <NavBar/>
+      <Home />
     </h1>
   );
 }
